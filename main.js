b73ba466d8a3f1036fbe94db06c8384758c7c31f
anajuliasilveira2009 <silveira.ana07@escola.pr.gov.br>
2025-09-19 07:46:24 -0300

    Valor entropia

--- main.js
+++ main.js
@@ -63,13 +63,17 @@ function geraSenha(){
     campoSenha.value = senha;
 }
 
-function classificaSenha(){
+function classificaSenha(tamanhoAlfaeto){
+    let entropia = tamanhoSenha * Math.log2(tamanhoAlfaeto):
+    console.log(entropia);
     forcaSenha.classList.remove('fraca', 'media', 'forte');
     if (tamanhoSenha > 11){
         forcaSenha.classList.add('forte');
-    } else if (tamanhoSenha > 5 && tamanhoSenha < 12){
+    } else if (entropia > 5 && entropia < 57){
         forcaSenha.classList.add('media');
-    }else if (tamanhoSenha <= 5){
+    }else if (entropia <= 35){
         forcaSenha.classList.add('fraca');
     }
+    const valorEntropia = document.querySelector('.entropia');
+    valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
 }
